402c78f9128086aeb7d9759294595333113bb011
Carsten Wilhelm <carsten.wilhelm@macaw.net>
2022-12-16 16:49:22 +0100

    Better description for configuration options

--- src/configuration.ts
+++ src/configuration.ts
@@ -12,47 +12,49 @@ export class Configuration {
     GM_config.init(
       {
         id: 'MacawUnit4Config',
+        title: 'Unit4 enhancements configuration',
         events: {
           save: () => this.save()
         },
         fields: {
           alwaysShowDescriptions: {
-            label: '[Global]: Always show descriptions for a workorder',
-            labelPos: 'right',
-            type: 'checkbox',
-            default: true
-          },
-          handleTimeEntry: {
-            label: 'Enhance Timesheet Entry',
-            labelPos: 'right',
-            type: 'checkbox',
-            default: true
-          },
-          handleTimesheetDetails: {
-            label: 'Enhance Timesheet Approval',
-            labelPos: 'right',
-            type: 'checkbox',
-            default: true
-          },
-          stickyWorkflowLog: {
-            label: 'Make Workflow Log sticky (in approval view)',
-            labelPos: 'right',
-            type: 'checkbox',
-            default: true
-          },
-          hideLockedRows: {
-            label: '[Timesheet Approval]: hide rows that you cannot accept/reject',
+            label: '[Global]: Always show descriptions for a workorder<copy>Display the name of the workorder and project whereever possible.</copy>',
             labelPos: 'right',
             type: 'checkbox',
             default: true
           },
           allowCommaEntry: {
-            label: '[Global]: allow time entry with "," as separator',
+            label: '[Global]: allow time entry with "," as separator<copy>Enable comma as decimal separator (in addition to the dot). Does not work consistently, so by default it is disabled</copy>',
             labelPos: 'right',
             type: 'checkbox',
             default: false
           },
-        }
+          handleTimeEntry: {
+            label: '[Timesheet Entry]: enable enhancements<copy>Enable enhancements on time entry screen</copy>',
+            labelPos: 'right',
+            type: 'checkbox',
+            default: true
+          },
+          handleTimesheetDetails: {
+            label: '[Timesheet Approval]: enable enhancements<copy>Enable enhancements on approval / rejection screen</copy>',
+            labelPos: 'right',
+            type: 'checkbox',
+            default: true
+          },
+          stickyWorkflowLog: {
+            label: '[Timesheet Approval]: Make Workflow Log sticky (in approval view)<copy>In Approval/Reject view, make the box with the log entry sticky, so it it visible all the time. This might lead to the box overlapping your time entries, so by default this is disabled</copy>',
+            labelPos: 'right',
+            type: 'checkbox',
+            default: true
+          },
+          hideLockedRows: {
+            label: '[Timesheet Approval]: hide rows that you cannot accept/reject<copy>In Approval screen, hide all rows that you are not responsible for</copy>',
+            labelPos: 'right',
+            type: 'checkbox',
+            default: true
+          },
+        },
+        css: 'copy { display: block; margin-left: 40px; font-weight: normal }'
       });
   }
 
