f006b6366f57a431552eea6cade063fb7efbaa3a
Carsten Wilhelm <carsten.wilhelm@macaw.net>
2023-05-05 18:09:18 +0200

    Version 0.9.24

--- dist/index.prod.user.js
+++ dist/index.prod.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        userscript-macaw-unit4
 // @namespace   https://ubw.unit4cloud.com/
-// @version     0.9.23
+// @version     0.9.24
 // @author      Carsten Wilhelm <carsten.wilhelm@macaw.net>
 // @source      https://github.com/macaw-cad/tampermonkey-unit4
 // @license     MIT
@@ -1051,6 +1051,29 @@ ___CSS_LOADER_EXPORT___.push([module.id, ".timesheetDetails {\n  box-sizing: bor
 
 /***/ }),
 
+/***/ "./node_modules/css-loader/dist/cjs.js!./node_modules/less-loader/dist/cjs.js!./src/modules/timesheetimport/timesheetimport.less":
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./node_modules/css-loader/dist/runtime/noSourceMaps.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, ".modalDialog {\n  position: fixed;\n  bottom: 0;\n  left: 0;\n  width: 100%;\n  height: 100%;\n  background: #ffffffcc;\n  box-sizing: border-box;\n  display: flex;\n  flex-direction: column;\n  justify-content: flex-start;\n  align-items: flex-start;\n  padding: 20px;\n  z-index: 9999;\n}\n.modalDialog textarea {\n  width: 100%;\n  height: 100%;\n  margin-bottom: 10px;\n  font-family: 'Courier New', Courier, monospace;\n  font-size: 16px;\n}\n.modalDialog__buttons {\n  margin-bottom: 10px;\n}\n.modalDialog button {\n  margin: 0 20px 0 0;\n  height: 40px;\n}\n", ""]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
 /***/ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js":
 /***/ ((module) => {
 
@@ -1445,7 +1468,7 @@ var __webpack_exports__ = {};
 "use strict";
 
 ;// CONCATENATED MODULE: ./package.json
-const package_namespaceObject = {"i8":"0.9.23"};
+const package_namespaceObject = {"i8":"0.9.24"};
 // EXTERNAL MODULE: ./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js
 var injectStylesIntoStyleTag = __webpack_require__("./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
 var injectStylesIntoStyleTag_default = /*#__PURE__*/__webpack_require__.n(injectStylesIntoStyleTag);
@@ -1568,6 +1591,12 @@ class Configuration {
           labelPos: 'right',
           type: 'checkbox',
           default: false
+        },
+        experimentalJsonImport: {
+          label: '[Timesheet Entry]: Import data from JSON<copy>This is an experimental feature for now and enabled to fill in workorders based on a JSON document</copy>',
+          labelPos: 'right',
+          type: 'checkbox',
+          default: false
         }
       },
       css: 'copy { display: block; margin-left: 40px; font-weight: normal }'
@@ -1619,6 +1648,10 @@ class Configuration {
 
   experimentalNewActionButtons() {
     return GM_config.get('experimentalNewActionButtons');
+  }
+
+  experimentalJsonImport() {
+    return GM_config.get('experimentalJsonImport');
   }
 
   show() {
@@ -2184,7 +2217,428 @@ class Global {
   }
 
 }
+// EXTERNAL MODULE: ./node_modules/css-loader/dist/cjs.js!./node_modules/less-loader/dist/cjs.js!./src/modules/timesheetimport/timesheetimport.less
+var timesheetimport = __webpack_require__("./node_modules/css-loader/dist/cjs.js!./node_modules/less-loader/dist/cjs.js!./src/modules/timesheetimport/timesheetimport.less");
+;// CONCATENATED MODULE: ./src/modules/timesheetimport/timesheetimport.less
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+
+var timesheetimport_options = {};
+
+timesheetimport_options.styleTagTransform = (styleTagTransform_default());
+timesheetimport_options.setAttributes = (setAttributesWithoutAttributes_default());
+
+      timesheetimport_options.insert = insertBySelector_default().bind(null, "head");
+    
+timesheetimport_options.domAPI = (styleDomAPI_default());
+timesheetimport_options.insertStyleElement = (insertStyleElement_default());
+
+var timesheetimport_update = injectStylesIntoStyleTag_default()(timesheetimport/* default */.Z, timesheetimport_options);
+
+
+
+
+       /* harmony default export */ const timesheetimport_timesheetimport = (timesheetimport/* default */.Z && timesheetimport/* default.locals */.Z.locals ? timesheetimport/* default.locals */.Z.locals : undefined);
+
+;// CONCATENATED MODULE: ./src/modules/timesheetimport/timesheetimport.ts
+function timesheetimport_defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+
+ // definition of a time entry with date ("M/D" format) and number of hours
+
+// status of currently imported work order
+var ImportWorkOrderStatus; // class for storing work orders to import
+
+(function (ImportWorkOrderStatus) {
+  ImportWorkOrderStatus[ImportWorkOrderStatus["ADD"] = 1] = "ADD";
+  ImportWorkOrderStatus[ImportWorkOrderStatus["WORKORDER"] = 2] = "WORKORDER";
+  ImportWorkOrderStatus[ImportWorkOrderStatus["ACTIVITY"] = 3] = "ACTIVITY";
+  ImportWorkOrderStatus[ImportWorkOrderStatus["DESCRIPTION"] = 4] = "DESCRIPTION";
+  ImportWorkOrderStatus[ImportWorkOrderStatus["TIME"] = 5] = "TIME";
+  ImportWorkOrderStatus[ImportWorkOrderStatus["DONE"] = 100] = "DONE";
+})(ImportWorkOrderStatus || (ImportWorkOrderStatus = {}));
+
+class ImportWorkOrder {}
+
+class Timesheetimport {
+  // max waiting time for a field to be available / get focus
+  // ----------------------------------------------------------------------
+  // Time Entry Screen Action Buttons
+  // ----------------------------------------------------------------------
+  constructor() {
+    timesheetimport_defineProperty(this, "active", false);
+
+    // add import button if this feature is enabled in configuration
+    document.querySelectorAll('h2.SectionTitle').forEach(e => {
+      if (Configuration.getInstance().experimentalJsonImport()) {
+        if (e.textContent.startsWith('Time entry')) {
+          let section = e.closest('.u4-section-placeholder');
+
+          if (section != null) {
+            this.active = true;
+            this.importButton(section);
+          }
+        }
+      }
+    });
+  }
+
+  isActive() {
+    return this.active;
+  }
+
+  importButton(tablesection) {
+    if (tablesection) {
+      const table = tablesection.querySelector('.TableButtonRow').closest('table');
+
+      if (table) {
+        //get Instance of original 'Add' btn
+        table.querySelectorAll('button').forEach(e => {
+          if (e.textContent.startsWith('Add')) {
+            this.standardAddBtn = e;
+          }
+        }); // create modal import dialog
+
+        this.dialog = document.createElement("div");
+        this.dialog.classList.add("modalDialog");
+        this.dialog.style.display = 'none';
+        this.dialogEntry = document.createElement("textarea");
+        this.dialog.appendChild(this.dialogEntry);
+        const dialogButtons = document.createElement("div");
+        dialogButtons.classList.add("modalDialog__buttons");
+        this.dialog.appendChild(dialogButtons);
+        const dialogOK = document.createElement("button");
+        dialogOK.setAttribute("type", "button");
+        dialogOK.classList.add("RibbonInlineButton", "RibbonInlineButtonHappy");
+        dialogOK.innerHTML = "<span>Start Import</span>";
+        dialogOK.addEventListener('click', this.actionImport.bind(this));
+        dialogButtons.appendChild(dialogOK);
+        const dialogCancel = document.createElement("button");
+        dialogCancel.setAttribute("type", "button");
+        dialogCancel.classList.add("RibbonInlineButton");
+        dialogCancel.innerHTML = "<span>Cancel</span>";
+        dialogCancel.addEventListener('click', this.actionClose.bind(this));
+        dialogButtons.appendChild(dialogCancel);
+        document.body.appendChild(this.dialog); //create new table cell
+
+        const buttonCell = table.rows[0].insertCell(1);
+        buttonCell.classList.add('Button'); //create new button
+
+        const button = document.createElement("button");
+        button.setAttribute("type", "button");
+        button.setAttribute("role", "button");
+        button.setAttribute("title", "Import data from JSON");
+        button.classList.add('BaseButton');
+        button.classList.add('SectionButton');
+        button.innerHTML = "<span>Impot JSON</span>";
+        button.addEventListener("click", this.actionDialog.bind(this));
+        buttonCell.appendChild(button); // Handle remaining data from last JSON import
+
+        this.handleImportNextItem();
+      }
+    }
+  } // show modal dialog
+
+
+  actionDialog() {
+    this.dialogEntry.value = '';
+    this.dialog.style.display = 'flex';
+  } // close modal dialog
+
+
+  actionClose() {
+    this.dialog.style.display = 'none';
+    this.dialogEntry.value = '';
+  } // start the import
+
+
+  actionImport() {
+    try {
+      const data = JSON.parse(this.dialogEntry.value);
+      /*
+      [
+        {
+          "workOrder": "400002-10027", "activity": "100", "description": "Import test #1",
+          "time": [
+            { "date": "5/1", "hours": "1.5" },
+            { "date": "5/2", "hours": "0.75" }
+          ]
+        },
+        {
+          "workOrder": "400002-10025", "activity": "100", "description": "Import test #2",
+          "time": [
+            { "date": "5/3", "hours": "1.25" },
+            { "date": "5/5", "hours": "4.75" }
+          ]
+        }
+      ]      
+      /*
+      // TODO: remove test data
+      const data: WorkOrder[] = [
+        {
+          workOrder: "400002-10027", activity: "100", description: "Import test #1",
+          time: [
+            { date: "5/1", hours: "1.5" },
+            { date: "5/2", hours: "0.75" },
+          ]
+        },
+        {
+          workOrder: "400002-10025", activity: "100", description: "Import test #2",
+          time: [
+            { date: "5/3", hours: "1.25" },
+            { date: "5/5", hours: "4.75" },
+          ]
+        }
+      ];
+      */
+      // put data in session and start with first entry
+
+      const next = data.shift();
+      sessionStorage.setItem("workorder_import_running", "true");
+      sessionStorage.setItem("workorder_import_next", JSON.stringify({
+        status: ImportWorkOrderStatus.ADD,
+        workOrder: next
+      }));
+      sessionStorage.setItem("workorder_import_pending", JSON.stringify(data)); // close dialog
+
+      this.actionClose(); // handle first import item
+
+      this.handleImportNextItem();
+    } catch (e) {
+      console.error(e);
+      alert("Import data must be valid JSON");
+    }
+  } // get the import object that is currently active
+
+
+  getCurrentImportWorkOrder() {
+    // do we have a current import object?
+    const rawNext = sessionStorage.getItem("workorder_import_next");
+
+    if (rawNext !== null && rawNext !== "") {
+      var next = JSON.parse(rawNext);
+
+      if (next.status === ImportWorkOrderStatus.DONE) {
+        // use next item from remaining list
+        next = this.getNextFromPending();
+      }
+
+      return next;
+    }
+  } // store the import object that is currently active (e.g. after status change)
+
+
+  storeCurrentImportWorkOrder(next) {
+    sessionStorage.setItem("workorder_import_next", JSON.stringify(next));
+  } // change status of import object and store in session
+
+
+  updateImportState(next, status) {
+    next.status = status;
+    this.storeCurrentImportWorkOrder(next);
+  } // get the next import object from the list of pending work orders
+
+
+  getNextFromPending() {
+    const raw = sessionStorage.getItem("workorder_import_pending");
+
+    if (raw !== null && raw !== "") {
+      const data = JSON.parse(raw);
+
+      if (data.length > 0) {
+        // get next item
+        const next = data.shift(); // update shortened list in session
+
+        sessionStorage.setItem("workorder_import_pending", JSON.stringify(data)); // return an import object with initial state
+
+        return {
+          status: ImportWorkOrderStatus.ADD,
+          workOrder: next
+        };
+      } else {
+        // no pending elements left
+        sessionStorage.setItem("workorder_import_pending", "");
+      }
+    }
+  } // wait a few seconds
+
+
+  wait(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+  } // wait for a input field to be focussed (queried by title of parent element)
+
+
+  async waitForFocus(title) {
+    var retries = Timesheetimport.retrySeconds;
+
+    do {
+      const act = document.activeElement;
+
+      if (act && act.nodeName === "INPUT" && act.parentElement.title === title) {
+        return act;
+      }
+
+      if (act && act.nodeName === "BUTTON" && act.title === title) {
+        return act;
+      }
+
+      await this.wait(1);
+    } while (--retries > 0);
+
+    throw new Error(`Input field not found for: ${title}`);
+  } // wait for element to be available (queried by CSS selector)
+
+
+  async waitForElements(query) {
+    var retries = Timesheetimport.retrySeconds;
+
+    do {
+      const res = this.standardAddBtn.ownerDocument.querySelectorAll(query);
+
+      if (res !== null && res.length > 0) {
+        return [...res];
+      }
+
+      await this.wait(1);
+    } while (--retries > 0);
+
+    throw new Error(`Element field not found for: ${query}`);
+  } // add a new row in timesheet
+
+
+  addNewRow(next) {
+    // next state is workorder
+    this.updateImportState(next, ImportWorkOrderStatus.WORKORDER); // click "Add" button
+    //console.log("Add a new row");
+
+    this.standardAddBtn.dispatchEvent(new Event('click')); // adding a row will reload the page
+
+    return true;
+  } // wait for workorder input in current row to get focus and fill in the given text
+
+
+  async fillWorkorder(next) {
+    // next state is description
+    this.updateImportState(next, ImportWorkOrderStatus.ACTIVITY); // fill in workorder
+
+    const input = await this.waitForFocus("Work order");
+    const curr = input.value;
+    input.value = next.workOrder.workOrder;
+    input.dispatchEvent(new KeyboardEvent('keydown', {
+      code: "Tab",
+      key: "Tab",
+      keyCode: 9,
+      which: 9,
+      bubbles: true,
+      cancelable: true
+    })); // page reloads if value has changed
+
+    return curr !== input.value;
+  } // wait for activity input in current row to get focus and fill in the given text
+
+
+  async fillActivity(next) {
+    // next state is workorder
+    this.updateImportState(next, ImportWorkOrderStatus.DESCRIPTION); //console.log("Fill in activity");
+
+    var input = await this.waitForFocus("Activity");
+    const curr = input.value;
+    input.value = next.workOrder.activity || "100";
+    input.dispatchEvent(new Event("blur")); // page reloads if value has changed
+
+    return curr !== input.value;
+  } // look for description area in current row and fill in the given text
+
+
+  async fillDescription(next) {
+    // next state is time entry
+    this.updateImportState(next, ImportWorkOrderStatus.TIME); //console.log("Fill in description", next.workOrder.description);
+
+    const input = this.standardAddBtn.ownerDocument.querySelector(".EditRow [data-type=cell-description] .InputCell input");
+    input.dispatchEvent(new Event("focus"));
+    input.value = next.workOrder.description;
+    input.dispatchEvent(new Event("blur")); // description changes NEVER trigger a page reload
+
+    return false;
+  } // look for next time entry with matching (date) title and fill in value
+
+
+  async fillTime(next) {
+    if (next.workOrder.time.length > 0) {
+      // handle next time entry
+      const entry = next.workOrder.time.shift();
+      this.storeCurrentImportWorkOrder(next);
+      const headers = await this.waitForElements('th[data-type=cell-weekday]');
+      const fields = await this.waitForElements('.EditRow [data-type=cell-weekday] .InputCell input');
+
+      for (var i = 0; i < headers.length; ++i) {
+        const head = headers[i];
+
+        if (head.title.includes(entry.date)) {
+          // seems to match the date
+          const field = fields[i];
+          const curr = field.value;
+          field.dispatchEvent(new Event("focus"));
+          field.value = `${entry.hours}`;
+          field.dispatchEvent(new Event("blur")); // page reloads if value has changed
+
+          return curr !== field.value;
+        }
+      }
+
+      return false;
+    } // set to done if there are no more time entries left
+
+
+    this.updateImportState(next, ImportWorkOrderStatus.DONE);
+    return false;
+  } // handle the import of the current import item
+
+
+  async handleImportNextItem() {
+    const next = this.getCurrentImportWorkOrder(); //console.log("Handle import", next);
+
+    if (next) {
+      var willReload = false; // run actions as long as we do not have a page reload
+
+      do {
+        if (next.status === ImportWorkOrderStatus.ADD) {
+          willReload = this.addNewRow(next);
+        } else if (next.status === ImportWorkOrderStatus.WORKORDER) {
+          willReload = await this.fillWorkorder(next);
+        } else if (next.status === ImportWorkOrderStatus.ACTIVITY) {
+          willReload = await this.fillActivity(next);
+        } else if (next.status === ImportWorkOrderStatus.DESCRIPTION) {
+          willReload = await this.fillDescription(next);
+        } else if (next.status === ImportWorkOrderStatus.TIME) {
+          willReload = await this.fillTime(next);
+        } else if (next.status === ImportWorkOrderStatus.DONE) {
+          // just reload the frame to finish
+          window.location.reload();
+          willReload = true;
+        } //console.log("Will the last action reload the page?", willReload);
+
+      } while (!willReload);
+    } else if (sessionStorage.getItem("workorder_import_running") === "true") {
+      // HOORAY! We are done!
+      sessionStorage.setItem("workorder_import_running", "false");
+      alert("JSON import finished");
+    }
+  }
+
+}
+
+timesheetimport_defineProperty(Timesheetimport, "retrySeconds", 20);
 ;// CONCATENATED MODULE: ./src/index.ts
+
 
 
 
@@ -2197,10 +2651,11 @@ class Unit4Enhancer {
     const timeEntry = new TimeEntry();
     const timeSheet = new TimeSheet();
     const timeSheetActions = new Timesheetactions();
+    const timeSheetImport = new Timesheetimport();
     const global = new Global();
     const version = package_namespaceObject.i8;
 
-    if (timeEntry.isActive() || timeSheet.isActive() || timeSheetActions.isActive() || global.isActive()) {
+    if (timeEntry.isActive() || timeSheet.isActive() || timeSheetActions.isActive() || timeSheetImport.isActive() || global.isActive()) {
       console.log("Unit4 enhancements " + version + " active ... ");
     }
 
